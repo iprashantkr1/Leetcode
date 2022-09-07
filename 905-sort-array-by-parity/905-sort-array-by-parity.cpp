68fae1d9b11f80286aa895cc10b3043c0e8969af
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-07 22:29:55 +0530

    Time: 3 ms (99.68%), Space: 16.2 MB (81.71%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -7,26 +7,17 @@ public:
         int i=0 , j=n-1;
         
         while(i<j){
+          
+            if(nums[i]%2==1 && nums[j]%2==0)
+                swap(nums[i],nums[j]);
+            
             if(nums[i]%2==0)
                 i++;
-            if(nums[j]%2!=0)
-                j--;
             
-            if(i<j && nums[i]%2!=0)
-            {
-                if(nums[j]%2==0)
-                {
-                    swap(nums[i],nums[j]);
-                    i++;
-                    j--;
-                }
-                else
-                {
-                    j--;
-                    //swap(nums[i],nums[j]);
-                }
-            }
+            if(nums[j]%2==1)
+                j--;
         }
+        
         return nums;
     }
 };
