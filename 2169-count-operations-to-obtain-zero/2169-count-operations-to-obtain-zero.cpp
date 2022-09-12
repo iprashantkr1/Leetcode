6c2e699ded4975670576ac0afd3563c211935f69
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-13 00:11:51 +0530

    Time: 4 ms (45.39%), Space: 5.8 MB (75.74%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        
+        if(!num1 || !num2)
+            return 0;
+        
+        int count=0;
+        
+        while(num1 && num2){
+            if(num1>=num2)
+                num1-=num2;
+            else
+                num2-=num1;
+            
+            count++;
+        }
+        return count;
+    }
+};
