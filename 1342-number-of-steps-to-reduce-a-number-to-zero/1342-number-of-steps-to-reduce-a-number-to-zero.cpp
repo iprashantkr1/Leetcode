d9de05cf7283bf12978356a9fa754814f48cc137
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-12 23:44:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.23%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
+        
+        //Recursive approach
+        
+        if(num==0)  return 0;
+        if(num==1)  return 1;
+        
+        return numberOfSteps(num/2) + (num%2==0 ? 1:2);
+        
+        
+        
+        
+        
+        /*
+        //Iterative Approach
+        
         int count=0;
         while(num!=0){
             if(num%2==0)
@@ -16,6 +31,7 @@ public:
                 
         }
         return count;
+        */
     }
 };
 
