e082e5fc2703173645b18e09fa7b72f2cf62447f
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-19 01:41:03 +0530

    Time: 6 ms (12.47%), Space: 5.8 MB (87.63%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -4,27 +4,18 @@ public:
     bool isPrime(int n)
     {
         if(n<=1)    return false;
-        
         if(n<=3)    return true;
-        
         if(n%2==0 || n%3==0)    return false;
-        
         for(int i=5;i*i<=n;i+=6)
             if(n%i==0 || n%(i+2)==0)    return false;
-        
         return true;
     }
     
     bool isThree(int n) 
     {
-        if(isPrime(n))  
+        if(n<=2)
             return false;
         
-        int ctr=2;
-        
-        for(int i=2;i<n && ctr<=3;i++)
-            ctr+= n%i==0 ;
-        
-        return ctr==3;
+        return isPrime(sqrt(n)) && (int)sqrt(n)*sqrt(n) == n ;
     }
 };
