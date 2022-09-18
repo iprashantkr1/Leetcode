16794c7a5881cbcc6e6759511d99c0aa6a17a0da
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-19 00:31:58 +0530

    Time: 3 ms (50.00%), Space: 5.9 MB (50.00%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int smallestEvenMultiple(int n) {
         
-        int res=n;
         if(n%2!=0)
-            res=2*n;
-        return res;
+            n*=2;
+        return n;
     }
 };
