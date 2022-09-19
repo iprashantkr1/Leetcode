ed564af41910aa9e433a728fac685df91e14c9ca
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2022-09-19 19:21:26 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (60.77%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -7,11 +7,10 @@ private:
     }
     
 public:
-    string gcdOfStrings(string str1, string str2) {
+    string gcdOfStrings(string str1, string str2) 
+    {
         
-        int l1=str1.length() , l2=str2.length();
-        
-        return (str1+str2 == str2+str1) ? str1.substr(0,gcd(l1,l2)) : "" ;
+        return (str1+str2 == str2+str1) ? str1.substr(0,gcd(size(str1),size(str2))) : "" ;
             
     }
 };
