c66777769d009581f111ecec9dd17aeb6abe75ce
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2023-09-25 22:56:01 +0530

    Time: 4 ms (48.11%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int n=s.size();
-        int sum=t[n];
+        char c=t[n];
         for(int i=0;i<n;i++)
-            sum+=t[i]-s[i];
+            c^=t[i]^s[i];
         
-        return char(sum);
+        return c;
     }
 };
