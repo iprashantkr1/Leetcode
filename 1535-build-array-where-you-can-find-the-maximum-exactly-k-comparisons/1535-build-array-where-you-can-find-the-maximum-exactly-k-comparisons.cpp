40855f6edbc1289c93e4c284c937284c811d4753
Prashant Kumar <67536443+iprashantkr1@users.noreply.github.com>
2023-10-07 22:24:24 +0530

    Time: 31 ms (81.57%), Space: 8.5 MB (43.32%) - LeetHub

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    long long ways[51][101][51];
+    const int MOD = 1e9 + 7;
+    
+    int numOfArrays(int n, int m, int k) {
+		
+        for (int j = 1; j <= m; j++) {
+                ways[1][j][1] = 1;
+        }
+        
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= m; b++) {
+                for (int c = 1; c <= k; c++) {
+                    long long s = 0;
+
+		            
+                    s = (s + b * ways[a - 1][b][c]) % MOD;
+                    
+		           
+                    for (int x = 1; x < b; x++) {
+						s = (s + ways[a - 1][x][c - 1]) % MOD;
+                    }
+					
+                    ways[a][b][c] = (ways[a][b][c] + s) % MOD;
+                }
+            }
+        }
+
+        long long ans = 0;
+        for (int j = 1; j <= m; j++) {
+            ans = (ans + ways[n][j][k]) % MOD;
+        }
+        
+        return int(ans);
+    }
+};
